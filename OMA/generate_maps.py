98c4f73630a1255e64babff0ba5e2556eeabca44
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-08-24 17:15:43 +0200

    mapping oma to refseq

--- OMA/generate_maps.py
+++ OMA/generate_maps.py
@@ -15,9 +15,22 @@ with gzip.open("gene2refseq.gz", 'rt') as gene2refseq:
         if line[5] != "-":
             mapped[line[5]] =  int(line[0])
 
-
 print("writing pickle...")
 
 with open('refseq_to_tax.pickle', 'wb') as handle:
     pickle.dump(mapped, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+mapped.clear()
+mapped = dict()
+
+with gzip.open("oma-refseq.txt.gz", 'rt') as oma_refseq:
+    next(oma_refseq)
+    next(oma_refseq)
+    for line in oma_refseq:
+        line = line.strip().split()
+        mapped[line[0]] =  line[1]
+
+print("writing pickle...")
+
+with open('oma_to_refseq.pickle', 'wb') as handle:
+    pickle.dump(mapped, handle, protocol=pickle.HIGHEST_PROTOCOL)
