2210c5def3e2942c2faeec4916966a769a089cb8
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-08-25 16:51:51 +0200

    little fix

--- scripts/shared_kmers.sh
+++ scripts/shared_kmers.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v jellyfish)" ]; then
 fi
 
 if [ "$#" -ne 3 ]; then
-    echo "run: ./shared_kmers.sh 31 seq1.fa seq2.fa "
+    echo "run: ./shared_kmers.sh <kmer_size> <file1.fa> <file2.fa>" >&2
     exit 2
 fi
 
@@ -16,12 +16,12 @@ SEQ1=$2
 SEQ2=$3
 
 echo "Processing ..."
-jellyfish count -m ${KSIZE} -s 100M -t 10 -C ${SEQ1} &> /dev/null
+jellyfish count -m ${KSIZE} -s 100M -t 10 -C ${SEQ1}
 jellyfish dump -t -c mer_counts.jf | sort | uniq | sed '/^[[:blank:]]*$/d' | awk -F '\t' '{print $1}' > seq1.kmers
-jellyfish count -m ${KSIZE} -s 100M -t 10 -C ${SEQ2} &> /dev/null
+jellyfish count -m ${KSIZE} -s 100M -t 10 -C ${SEQ2}
 jellyfish dump -t -c mer_counts.jf | sort | uniq | sed '/^[[:blank:]]*$/d' | awk -F '\t' '{print $1}' > seq2.kmers
 
 SHARED=$(awk 'a[$0]++' seq1.kmers seq2.kmers | wc -l)
-# rm mer_counts.jf seq1.kmers seq2.kmers
+rm mer_counts.jf seq1.kmers seq2.kmers
 
 echo "${SEQ1} & ${SEQ2} have ${SHARED} shared unique kmers"
