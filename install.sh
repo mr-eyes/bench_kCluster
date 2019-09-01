0ad5a9db46ea990f6ab8139c36a635555d0aca55
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-09-02 00:50:36 +0200

    automated installation script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PROCESSING="\e[33m[RUNNING] \e[0m"
+OK="\e[32m[OK] \e[0m"
+
+if ! [ -x "$(command -v kCluster)" ]; then
+  echo 'kCluster is not installed.' >&2
+  echo -e "${PROCESSING} installing .." >&2
+  pip install git+https://github.com/mr-eyes/kCluster#egg=kCluster
+
+else
+    echo -e "${OK} kCluster exist, skipping.."
+fi
+
+
+FILE=./kCluster2
+if [ -d "$FILE" ]; then
+    echo -e "${OK} kCluster2 (pairwise) exist, skipping.."
+else 
+    git clone https://github.com/mr-eyes/kCluster2.git
+    cd kCluster2
+    git submodule update --init --recursive
+    mkdir build && cd build
+    cmake ..
+    make
+fi
+
+echo -e "---------"
+echo -e "${OK} Everthing has been installed successfully"
+echo ""
+echo "Test by invoking: ./kCluster2/build/kCluster_pairwise"
